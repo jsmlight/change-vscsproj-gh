a335f728014f994ef5185433c03258b836d0c15e
jsmlight <mlight.js@gmail.com>
2021-11-16 18:18:55 +0900

    20211116-작업본

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,315 @@ namespace change_vscsproj_gh
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tableLayoutPanel2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(778, 194);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.panel3, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.panel4, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.panel5, 2, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 17);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(772, 174);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "  Source Folder :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 43);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "  Target Folder :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 43);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "  Change TOOL version :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(3, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(194, 45);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "  Change .NET Version :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel1
+            // 
+            this.tableLayoutPanel2.SetColumnSpan(this.panel1, 2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(203, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(566, 37);
+            this.panel1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(481, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(472, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.tableLayoutPanel2.SetColumnSpan(this.panel2, 2);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(203, 46);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(566, 37);
+            this.panel2.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(481, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 9);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(472, 21);
+            this.textBox2.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 203);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(778, 355);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 17);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(772, 335);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.comboBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(203, 89);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(280, 37);
+            this.panel3.TabIndex = 10;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.comboBox2);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(203, 132);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(280, 39);
+            this.panel4.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(277, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(0, 10);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(277, 20);
+            this.comboBox2.TabIndex = 0;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.button3);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(489, 89);
+            this.panel5.Name = "panel5";
+            this.tableLayoutPanel2.SetRowSpan(this.panel5, 2);
+            this.panel5.Size = new System.Drawing.Size(280, 82);
+            this.panel5.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(267, 72);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "CHANGE CSPROJ FILE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Change csproj File Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,192 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
 
 namespace change_vscsproj_gh
 {
     public partial class Form1 : Form
     {
+        string sourceFolder = string.Empty;
+        string targetFolder = string.Empty;
+        string toolVersion = string.Empty;
+        string netVersion = string.Empty;
+
+        List<string> sourceFolderList;
+        List<string> csprojList;
+
         public Form1()
         {
             InitializeComponent();
+
+            sourceFolderList = new List<string>();
+            csprojList = new List<string>();
+
+
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new CommonOpenFileDialog())
+            {
+                dialog.IsFolderPicker = true;
+                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    textBox1.Text = dialog.FileName;
+                }
+            }
+
+        }
+
+        // tool version 변경
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string _inputVal = comboBox1.SelectedItem.ToString().Trim();
+            _inputVal = _inputVal.Split('|')[0].ToString();
+            toolVersion = _inputVal;
+        }
+
+        // .net version 변경
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string _inputVal = comboBox2.SelectedItem.ToString().Trim();
+            _inputVal = _inputVal.Split('|')[0].ToString();
+            netVersion = _inputVal;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1_init();
+            comboBox2_init();
+        }
+
+        public void comboBox1_init()
+        {
+            comboBox1.Items.Add("17.0 | VisualStudio 2022");
+            comboBox1.Items.Add("16.0 | VisualStudio 2019");
+            comboBox1.Items.Add("15.0 | VisualStudio 2017");
+            comboBox1.Items.Add("14.0 | VisualStudio 2015");
+            comboBox1.Items.Add("12.0 | VisualStudio 2013");
+            comboBox1.Items.Add("11.0 | VisualStudio 2012");
+            comboBox1.Items.Add("10.0 | VisualStudio 2010");
+            comboBox1.Items.Add(" 9.0 | VisualStudio 2008");
+            comboBox1.Items.Add(" 8.0 | VisualStudio 2005");
+            comboBox1.Items.Add(" 7.0 | VisualStudio .NET(2002)");
+            comboBox1.Items.Add(" 6.0 | VisualStudio 6.0");
+            comboBox1.Items.Add(" 5.0 | VisualStudio 97");
+        }
+
+        public void comboBox2_init()
+        {
+            comboBox2.Items.Add("v4.8 | VisualStudio 2019 later");
+            comboBox2.Items.Add("v4.7.2 | VisualStudio 2017 later");
+            comboBox2.Items.Add("v4.7.1 | VisualStudio 2017 later");
+            comboBox2.Items.Add("v4.7 | VisualStudio 2017 later");
+            comboBox2.Items.Add("v4.6.2 | VisualStudio 2017 later");
+            comboBox2.Items.Add("v4.6.1 | VisualStudio 2015 later");
+            comboBox2.Items.Add("v4.6 | VisualStudio 2015 later");
+            comboBox2.Items.Add("v4.5.1 | VisualStudio 2013 later");
+            comboBox2.Items.Add("v4.5 | VisualStudio 2012 later");
+            comboBox2.Items.Add("v4.0 | VisualStudio 2010 later");
+            comboBox2.Items.Add("v3.5 | VisualStudio 2008 later");
+            comboBox2.Items.Add("v3.0 | VisualStudio 2008 later");
+            comboBox2.Items.Add("v2.0 | VisualStudio 2005 later");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+
+            if (String.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("sourceFolder is null or empty");
+            }
+            else 
+            {
+                sourceFolder = Path.GetFullPath(textBox1.Text.Trim());
+            }
+
+            if (String.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("원본폴더에 내용이 수정됨으로 타겟폴더를 지정해주세요.");
+            }
+            else 
+            {
+                targetFolder = Path.GetFullPath(textBox2.Text.Trim());
+            }
+
+            //csproj 파일 찾기
+            foreach (var item in GetSearchCsprojFile(sourceFolder))
+            {
+                
+                richTextBox1.AppendText(item + Environment.NewLine);
+
+                //파일 변경
+                ChangeCsprojFile(Path.GetFullPath(item), toolVersion, netVersion);
+            }
+            
+
+
+        }
+
+
+        public string[] GetSearchCsprojFile(String _strPath) 
+        { 
+            string[] files = { "", }; 
+            try { 
+                files = Directory.GetFiles(_strPath, "*.csproj", SearchOption.AllDirectories); 
+            } 
+            catch (IOException ex) 
+            { 
+                MessageBox.Show(ex.Message); 
+            } 
+            return files; 
+        }
+
+        public string ChangeCsprojFile(String _file, String _toolver, String _netver)
+        {
+            if (String.IsNullOrEmpty(_toolver) && String.IsNullOrEmpty(_netver))
+                return "변경된 파일이 없습니다.";
+
+            string[] all_lines = File.ReadAllLines(_file);
+
+            int index = 0;
+            foreach (string line in all_lines)
+            {
+                if (!String.IsNullOrEmpty(_toolver) && line.Contains("ToolsVersion="))
+                {
+                    int _first = line.IndexOf("ToolsVersion=\"");
+                    int first_location = _first + "ToolsVersion=".Length + 1;
+                    int last_location = line.IndexOf("\">", first_location);
+                    int replace_word_count = last_location - first_location;
+
+                    string before_replace_word = line.Substring(first_location, replace_word_count);
+
+                    all_lines[index] = line.Replace(before_replace_word, _toolver);
+                }
+                if (!String.IsNullOrEmpty(_netver) && line.Contains("<TargetFrameworkVersion>"))
+                {
+                    int _first = line.IndexOf("<TargetFrameworkVersion>");
+                    int first_location = _first + "<TargetFrameworkVersion>".Length;
+                    int last_location = line.IndexOf("</TargetFrameworkVersion>", first_location);
+                    int replace_word_count = last_location - first_location;
+
+                    string before_replace_word = line.Substring(first_location, replace_word_count);
+
+                    all_lines[index] = line.Replace(before_replace_word, _netver);
+                }
+                index++;
+            }
+
+
+
+            File.WriteAllLines(_file, all_lines);
+
+            return _file;
+        }
+            
+
+
     }
 }
